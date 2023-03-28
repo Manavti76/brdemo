88bc913e6d41670644801652249f43c9cdb043ff
Manavti76 <jfsjdpune02.manavtiwari@gmail.com>
2023-03-28 10:28:31 +0530

    first commit

---
+++ R1.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+class R1 {
+	public static void main(String args[])
+{
+System.out.println("1st Random Number: in the program" + Math.random());
+System.out.println("2st Random Number: in the program" + Math.random());
+
+}
+
+}
+
